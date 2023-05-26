b6d0a90a3a3d58f196136d2d0e4060a3388b08ea
OmarHory <o_hawary@hotmail.com>
2023-05-26 17:00:33 +0300

    append

--- src/models/data.py
+++ src/models/data.py
@@ -85,7 +85,9 @@ def append_data(csv_name, table_name, engine):
                 insert_query = f"INSERT INTO {table_name} (date, hour, day_name, is_weekend, month, year, type, source, cashflow) VALUES ('{pd.to_datetime(row['date'])}', '{row['hour']}', '{row['day_name']}', '{row['is_weekend']}', '{row['month']}', '{row['year']}', '{row['type']}', '{row['source']}', '{row['cashflow']}')"
                 session.execute(insert_query)
             except Exception as e_:
-                print(e_)
+                print('An error has occurred on inserting date {} - hour {} - type {} - source {} - cashflow {}'.format(row['date'], row['hour'], row['type'], row['source'], row['cashflow']))
+
+        print("done")
     
 
     session.commit()
