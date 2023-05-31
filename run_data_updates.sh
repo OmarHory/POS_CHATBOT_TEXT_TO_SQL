ec60a52909538736d13c463687f637d6b319eea0
Ubuntu <ubuntu@ip-172-31-11-10.eu-north-1.compute.internal>
2023-05-31 16:30:14 +0000

    edits

--- run_data_updates.sh
+++ run_data_updates.sh
@@ -1,6 +1,7 @@
 cd /home/ubuntu/chicks_project/foodics_gpt/
 . .venv/bin/activate
 
-jupyter nbconvert --execute data_notebooks/orders/update_orders_data.ipynb --to python --inplace
+jupyter nbconvert --execute data_notebooks/orders/prepare_data.ipynb --to python --inplace
+jupyter nbconvert --execute data_notebooks/orders/process_tables.ipynb --to python --inplace
 
-python src/models/runner/data_runner_update.py
+python src/models/runner/data_runner.py
