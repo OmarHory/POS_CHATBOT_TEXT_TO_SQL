ce3b63263010d7caf55c037daf664512a4f8ea3e
OmarHory <o_hawary@hotmail.com>
2023-05-31 16:38:18 +0300

    sorry gpt temp edit

--- src/response_builder.py
+++ src/response_builder.py
@@ -186,7 +186,7 @@ def process_send_gpt(
     #     any(item in response.lower() for item in sorry_words)
     #     and "undefined" not in edited_intent.lower()
     # ):
-    if ("undefined" in edited_intent.lower()):
+    if ("undefined" not in edited_intent.lower()):
         print("Go to SQL GPT")
         incoming_msg = edit_prompt(incoming_msg)
 
