fee50bb038b6b271ab7ed36145b0ffac71a3a4b9
OmarHory <o_hawary@hotmail.com>
2023-06-09 21:57:16 +0300

    data updates

---
+++ data_notebooks/orders/pull_orders.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import requests
+import time
+from datetime import date
+pd.set_option('display.max_columns', None)
+import os
+
+from dotenv import load_dotenv
+from utils import *
+
+# Load environment variables from .env file
+load_dotenv()
+
+filter = {}
+
+business_name = os.getenv('business_name')
+if business_name == 'Blk Coffee':
+    print(f"Yes it is {business_name}")
+    filter = {"business_date_after": "2023-05-01"}
+
+
+print('orders_final_include.csv does not exist')
+includables = 'products,branch,products.product,tags,products.promotion,payments.payment_method'
+# call the foodics api to get the orders
+last_page = call_foodics('orders', 1, includables=includables, filter=filter, return_last_page=True)
+print('last_page: ', last_page)
+
+list_responses = call_foodics('orders', last_page, includables=includables, filter=filter)
+df_orders = pd.DataFrame([item for sublist in list_responses for item in sublist])
+df_orders.to_csv('data/orders_final_include.csv', index=False)
+print(df_orders.shape)
+
---
+++ data_notebooks/orders/utils.py
@@ -0,0 +1,68 @@
+import os
+import requests
+import time
+import pandas as pd
+
+def foodics_api(method, payload={}):
+    
+  url = f"https://api.foodics.com/v5/{method}"
+  token = os.getenv('data_access_token')
+  headers = {
+    'Authorization': f'Bearer {token}',
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
+
+  response = requests.request("GET", url, headers=headers, data=payload)
+  if response.status_code != 200:
+        return response.status_code
+
+  return response.json()
+
+
+def call_foodics(method, last_page, includables=None, filter = {}, return_last_page=False, from_page=1, checkpoint_path='data/orders_final_include.csv'):
+    
+    list_responses = []
+    counter = 0
+    
+    for page in range(from_page, last_page+1):
+        print(f"page {page}")
+        retries = 3
+        success = False
+
+        while not success and retries > 0:
+            method_ = f'{method}?page={page}'
+            try:
+                if includables is not None:
+                    method_ = method_ + f'&include={includables}'
+                if len(filter):
+                    filters = ''
+                    for key, val in filter.items():
+                        filters = f'&filter[{key}]={val}'
+                    method_ = method_ + filters
+                print(method_)
+
+                response = foodics_api(method_)
+                
+                # If the request is successful, the following line will be executed
+                if return_last_page:
+                    return response['meta']['last_page']
+                list_responses.append(response['data'])
+                success = True
+            except Exception as e:
+                print(f"Request failed with page: {page} {str(e)}, retrying... {retries} retries left.")
+                retries -= 1
+                time.sleep(70) # wait 70 seconds before retrying
+        if counter == 50 and method == 'orders':
+            print("Writing to path.")
+            pd.DataFrame([item for sublist in list_responses for item in sublist]).to_csv(checkpoint_path, index=False)
+            counter = 0
+        
+
+        if not success:
+            print(f"Failed to retrieve data for page {page} after 3 retries.")
+            continue
+
+        counter+=1
+
+    return list_responses
