306297c51a0908b524e61f52565a089a76559543
OmarHory <o_hawary@hotmail.com>
2023-06-14 01:15:24 +0300

    refactor s3 operations

--- run_notification.sh
+++
@@ -1,9 +0,0 @@
-cd /home/ubuntu/chicks_project/foodics_gpt/
-. .venv/bin/activate
-
-
-if [ "$push_notification_var" = "reminder" ]; then
-    python src/push_notification.py --option reminder
-else
-    python src/push_notification.py --option report
-fi
--- s3_operations.py
+++ s3_operations.py
@@ -1,47 +1,42 @@
 import boto3
-from src.config import config_aws
 import argparse
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-aws_access_key = os.getenv("aws_access_key")
-aws_secret_access_key = os.getenv("aws_secret_access_key")
-aws_bucket_name = os.getenv("aws_bucket_name")
-aws_folder_name = os.getenv("aws_s3_folder")
-client_id = os.getenv("client_id")
+def main():
+    aws_access_key = os.getenv("aws_access_key")
+    aws_secret_access_key = os.getenv("aws_secret_access_key")
+    aws_bucket_name = os.getenv("aws_bucket_name")
+    aws_folder_name = os.getenv("aws_s3_folder")
+    client_id = os.getenv("client_id")
 
+    parser = argparse.ArgumentParser(description="Process method and file names arguments")
+    parser.add_argument("method", choices=["download", "upload"], help="Specify the method: download or upload")
+    parser.add_argument("file_names", nargs="+", help="Specify the CSV file names")
+    args = parser.parse_args()
+    method = args.method
+    file_names = args.file_names
 
-parser = argparse.ArgumentParser(description="Process method argument")
+    s3 = boto3.client("s3", aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_access_key)
 
-parser.add_argument(
-    "method",
-    choices=["download", "upload"],
-    help="Specify the method: download or upload",
-)
+    if method == "download":
+        print("Performing download...")
+        download_files(s3, aws_bucket_name, aws_folder_name, client_id, file_names)
+    elif method == "upload":
+        print("Performing upload...")
+        upload_files(s3, aws_bucket_name, aws_folder_name, client_id, file_names)
+    else:
+        raise ValueError("Invalid method specified")
 
-args = parser.parse_args()
+def download_files(s3, bucket_name, folder_name, client_id, file_names):
+    for name in file_names:
+        s3.download_file(bucket_name, f"{folder_name}/{name}", f"data/{client_id}/raw/{name}")
 
-method = args.method
+def upload_files(s3, bucket_name, folder_name, client_id, file_names):
+    for name in file_names:
+        s3.upload_file(f"data/{client_id}/raw/{name}", bucket_name, f"{folder_name}/{name}")
 
-s3 = boto3.client(
-    "s3", aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_access_key
-)
-
-if method == "download":
-    print("Performing download...")
-
-    for name in ["orders_final_include.csv"]:
-        s3.download_file(aws_bucket_name, f"{aws_folder_name}/{name}", f"data/{client_id}/raw/{name}")
-elif method == "upload":
-    print("Performing upload...")
-    for name in ["orders_final_include_updated.csv"]:
-        s3.upload_file(
-            f"data/{client_id}/raw/{name}",
-            aws_bucket_name,
-            f"{aws_folder_name}/orders_final_include.csv",
-        )
-
-else:
-    raise ValueError("Invalid method specified")
+if __name__ == "__main__":
+    main()
