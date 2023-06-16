9d47d270f7f64ee537d96e9026fce1579b50032d
OmarHory <o_hawary@hotmail.com>
2023-06-16 13:54:49 +0300

    edit pull orders

--- data_notebooks/orders/pull_orders.py
+++ data_notebooks/orders/pull_orders.py
@@ -1,25 +1,26 @@
-import pandas as pd
-import requests
-import time
-from datetime import date
-pd.set_option('display.max_columns', None)
 import os
-
+import argparse
 from dotenv import load_dotenv
-from utils import *
+from utils import call_foodics
 
 # Load environment variables from .env file
 load_dotenv()
 
-filter = {}
+# Parse command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--client-id', type=int, required=True, help='Client ID')
+args = parser.parse_args()
 
-print('orders_final_include.csv does not exist')
-includables = 'products,branch,products.product,tags,products.promotion,payments.payment_method,products.promotion,products.options,products.options.modifier_option'
-# call the foodics api to get the orders
-last_page = call_foodics('orders', 1, includables=includables, filter=filter, return_last_page=True)
-print('last_page: ', last_page)
+# Get environment variables
+token = os.getenv('data_access_token')
+client_id = args.client_id
 
-list_responses = call_foodics('orders', last_page, includables=includables, filter=filter)
-df_orders = pd.DataFrame([item for sublist in list_responses for item in sublist])
-df_orders.to_csv('data/orders_final_include.csv', index=False)
-print(df_orders.shape)
+# Set filter
+filter_ = {}
+
+includables = 'branch,products.product,products.options.modifier_option'
+# Call the foodics api to get the orders
+last_page = call_foodics('orders', 1, client_id, token, includables=includables, filter=filter_, return_last_page=True)
+print('last_page:', last_page)
+
+call_foodics('orders', last_page, client_id, token, includables=includables, filter=filter_, checkpoint_every=2)
