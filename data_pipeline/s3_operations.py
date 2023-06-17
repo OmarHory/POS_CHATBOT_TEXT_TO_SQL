94e72ea06d0d158518af0e12caaba4b40e79efbd
OmarHory <o_hawary@hotmail.com>
2023-06-17 12:15:13 +0300

    S3 Handler changes

--- data_pipeline/s3_operations.py
+++ data_pipeline/s3_operations.py
@@ -1,5 +1,5 @@
-
-import os, sys
+import os
+import sys
 from dotenv import load_dotenv
 
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
@@ -16,15 +16,16 @@ parser.add_argument('--client-id', type=int, required=True, help='Client ID')
 parser.add_argument('--method', type=str, required=True, help='Method to run')
 parser.add_argument('--local_data_folder', type=str, required=True, help='local_data_folder to do operation -> raw, processed, updates')
 parser.add_argument('--aws_data_folder', type=str, required=True, help='aws_data_folder to do operation -> raw, processed, updates')
-
+parser.add_argument('--files', nargs='+', type=str, help='Specific file names to download or delete')
 
 args = parser.parse_args()
 client_id = args.client_id
 method = args.method
 local_data_folder = args.local_data_folder
 aws_data_folder = args.aws_data_folder
+files = args.files
 
-#Environment Variables
+# Environment Variables
 aws_access_key = os.getenv("aws_access_key")
 aws_secret_access_key = os.getenv("aws_secret_access_key")
 bucket_name = os.getenv("aws_bucket_name")
@@ -34,16 +35,28 @@ local_path = f"data/{client_id}/{local_data_folder}"
 if method not in ['upload', 'download', 'delete']:
     raise Exception('Invalid method')
 
-csv_files = list_csv_files_in_directory(f'data/{client_id}/{local_data_folder}')
-print(csv_files)
-
-if len(csv_files) == 0:
-    raise Exception('No csv files found')
-
 handler = S3Handler(aws_access_key, aws_secret_access_key)
+
 if method == 'upload':
+    csv_files = list_csv_files_in_directory(f'data/{client_id}/{local_data_folder}')
+    if len(csv_files) == 0:
+        raise Exception('No CSV files found')
     handler.upload_files(bucket_name, aws_folder_path, local_path, csv_files)
+
 elif method == 'download':
-    handler.download_files(bucket_name, aws_folder_path, local_path, csv_files)
+    if files is None:
+        files = handler.list_files(bucket_name, aws_folder_path)
+
+    if len(files) == 0:
+        raise Exception('No files found in S3')
+
+    handler.download_files(bucket_name, aws_folder_path, local_path, files)
+
 elif method == 'delete':
-    handler.delete_files(bucket_name, aws_folder_path, csv_files)
+    if files is None:
+        files = handler.list_files(bucket_name, aws_folder_path)
+
+    if len(files) == 0:
+        raise Exception('No files found in S3')
+
+    handler.delete_files(bucket_name, aws_folder_path, files)
--- src/s3_handler.py
+++ src/s3_handler.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.exceptions import ClientError
 
 
 class S3Handler:
@@ -11,9 +12,15 @@ class S3Handler:
 
     def download_files(self, bucket_name, folder_name, local_path, file_names):
         for name in file_names:
-            self.s3.download_file(
-                bucket_name, f"{folder_name}/{name}", f"{local_path}/{name}"
-            )
+            try:
+                self.s3.download_file(
+                    bucket_name, f"{folder_name}/{name}", f"{local_path}/{name}"
+                )
+            except ClientError as e:
+                if e.response["Error"]["Code"] == "404":
+                    print(f"File {name} does not exist in the S3 bucket.")
+                else:
+                    print(f"Error downloading file {name}: {e}")
 
     def upload_files(self, bucket_name, folder_name, local_path, file_names):
         for name in file_names:
@@ -30,3 +37,12 @@ class S3Handler:
 
     def delete_folder(self, bucket_name, folder_name):
         self.s3.delete_object(Bucket=bucket_name, Key=folder_name)
+
+    def list_files(self, bucket_name, folder_name):
+        response = self.s3.list_objects_v2(Bucket=bucket_name, Prefix=folder_name)
+        files = []
+        if "Contents" in response:
+            for obj in response["Contents"]:
+                file_name = obj["Key"].split("/")[-1]
+                files.append(file_name)
+        return files
