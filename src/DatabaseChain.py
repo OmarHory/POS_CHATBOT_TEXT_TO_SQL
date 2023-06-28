67a6724d1d0106f2ac11d893cade28256104c06b
OmarHory <o_hawary@hotmail.com>
2023-06-28 18:32:22 +0300

    add it to format

--- src/DatabaseChain.py
+++ src/DatabaseChain.py
@@ -112,7 +112,7 @@ class SQLDatabaseChain(Chain, BaseModel):
         try:
             result = self.database.run(sql_cmd)
         except Exception as e:
-            prompt_revision = sql_revision_prompt.format(e)
+            prompt_revision = sql_revision_prompt.format(e, self.properties['user_question'])
             print("\nPrompt revision:\n", prompt_revision, "\n")
             sql_cmd = send_to_gpt(prompt_revision)
             print("New sql_cmd:\n", sql_cmd)
